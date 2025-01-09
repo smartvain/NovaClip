6851faacc749c76a63e82ab1d8ba2ebde55691f6
YULE-WINDOWS\zlcbl <zlc.blue15210@gmail.com>
2025-01-09 23:42:12 +0900

    add: 動画生成コンポーネントを配置

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import { Provider } from "@/components/ui/provider";
+import { VideoConverter } from "@/components/ui/video-converter";
 
 export default function Home() {
   return (
     <Provider>
-      <div>Hello World</div>
+      <VideoConverter />
     </Provider>
   );
 }
