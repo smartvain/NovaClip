d3e699c7ad553bba32c5880272927bda265ed3c6
YULE-WINDOWS\zlcbl <zlc.blue15210@gmail.com>
2025-01-11 17:32:56 +0900

    eslint: インポート順の設定追加

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
+
 import { FlatCompat } from '@eslint/eslintrc'
+import eslintPluginImport from 'eslint-plugin-import'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -12,8 +14,11 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
+    plugins: {
+      import: eslintPluginImport,
+    },
     rules: {
-      semi: ['error', 'never'], // セミコロンを禁止
+      semi: ['error', 'never'],
       'semi-spacing': ['error', { before: false, after: true }],
       quotes: [
         'error',
@@ -33,6 +38,24 @@ const eslintConfig = [
           ignoreTemplateLiterals: true,
           ignoreRegExpLiterals: true,
           ignoreComments: false,
+        },
+      ],
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', ['parent', 'sibling'], 'index', 'type'],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+          pathGroups: [
+            {
+              pattern: '@/**',
+              group: 'internal',
+              position: 'after',
+            },
+          ],
         },
       ],
     },
