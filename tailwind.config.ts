6a3e336e966db3dab16c180b55537aa3fe647e52
YULE-WINDOWS\zlcbl <zlc.blue15210@gmail.com>
2025-01-11 19:38:19 +0900

    change: tailwindでフォント管理する

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,5 +7,13 @@ export default {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-geist-sans)', 'Arial', 'sans-serif'],
+        mono: ['var(--font-geist-mono)', 'monospace'],
+      },
+    },
+  },
   plugins: [],
 } satisfies Config
