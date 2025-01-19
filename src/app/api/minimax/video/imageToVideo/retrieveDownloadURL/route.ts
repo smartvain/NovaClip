02ce45b6620c17edadb2b759e89e1e58b6bc3a70
YULE-WINDOWS\zlcbl <zlc.blue15210@gmail.com>
2025-01-19 22:02:54 +0900

    add: get MINIMAX_GROUP_ID

--- src/app/api/minimax/video/imageToVideo/retrieveDownloadURL/route.ts
+++ src/app/api/minimax/video/imageToVideo/retrieveDownloadURL/route.ts
@@ -3,23 +3,24 @@ import { NextResponse } from 'next/server'
 import { MINIMAX_API_ENDPOINTS } from '@/constants/endpoints'
 import { requestToMinimax } from '@/lib/api/minimax/common'
 
+const MINIMAX_GROUP_ID = process.env.MINIMAX_GROUP_ID
+
 export async function GET(request: Request) {
   try {
     const { searchParams } = new URL(request.url)
     const fileId = searchParams.get('file_id')
-    const groupId = searchParams.get('GroupId')
 
     if (!fileId) {
       return NextResponse.json({ error: 'file_id is required' }, { status: 400 })
     }
-    if (!groupId) {
+    if (!MINIMAX_GROUP_ID) {
       return NextResponse.json({ error: 'GroupId is required' }, { status: 400 })
     }
 
     const url = MINIMAX_API_ENDPOINTS.IMAGE_TO_VIDEO.RETRIEVE_DOWNLOAD_URL.replace(
       '{file_id}',
       fileId!
-    ).replace('{GroupId}', groupId!)
+    ).replace('{GroupId}', MINIMAX_GROUP_ID!)
 
     const response = await requestToMinimax(url, { method: 'GET' })
 
