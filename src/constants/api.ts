7ad97c7b23f6bde5703b09542c177e8c733697c9
YULE-WINDOWS\zlcbl <zlc.blue15210@gmail.com>
2025-01-20 03:44:08 +0900

    add: minimax polling

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -5,14 +5,14 @@ export const API_STATUS_CODES = {
   UNAUTHORIZED: 401,
 } as const
 
-export const MINIMAX_BASE_RESP_STATUS_CODES = {
+export const MINIMAX_TASK_STATUS_CODES = {
   PREPARING: 'Preparing',
   PROCESSING: 'Processing',
   SUCCESS: 'Success',
   FAIL: 'Fail',
 } as const
 
-export const MINIMAX_TASK_STATUS_CODES = {
+export const MINIMAX_BASE_RESP_STATUS_CODES = {
   SUCCESS: 0,
   TRIGGER_RATE_LIMIT: 1002,
   ACCOUNT_AUTHENTICATION_FAILED: 1004,
